c8eb469842a89f846981dcb235c6152c0d16201b
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2023-11-28 10:06:40 +0100

    qde

--- middleware.js
+++ middleware.js
@@ -1,7 +1,12 @@
 export default function middleware(request) {
   const url = new URL(request.url);
 
-  // Condizioni di reindirizzamento
+  // Ignora le richieste per le risorse statiche come immagini, CSS, JS, ecc.
+  if (url.pathname.match(/\.(jpg|jpeg|png|gif|css|js)$/)) {
+    return; // Continua a servire la risorsa dal dominio attuale
+  }
+
+  // Reindirizza tutte le altre pagine
   if (url.pathname !== "/") {
     return Response.redirect(`https://serp-secrets.com${url.pathname}`, 301);
   }
