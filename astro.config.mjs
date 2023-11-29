64493508c8e93c85fabc7e2c94884415e39c0de5
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2023-11-29 22:33:49 +0100

    ed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,36 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
+
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  site: 'https://www.filippodanesi.it',
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+        },
+        {
+          userAgent: 'GPTBot',
+          disallow: '/',
+        },
+        {
+          userAgent: 'CCBot',
+          disallow: '/',
+        },
+        {
+          userAgent: 'Google-Extended',
+          disallow: '/',
+        },
+        {
+          userAgent: 'anthropic-ai',
+          disallow: '/',
+        },
+      ],
+      sitemap: 'https://www.filippodanesi.it/sitemap.xml',
+    })
+  ]
 });
